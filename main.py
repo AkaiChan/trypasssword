866e5cb00e23c75502ea61df7d78903e88c9be87
Bruce Chan <bruce20219@gmail.com>
2019-12-01 20:41:30 +0800

    Improvment

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 strpasword = "a123456"
 i = 3
 while i > 0:
-	strinput = input("Please input password")
+	i = i - 1
+	strinput = input("Please input password: ")
 	if strinput == strpasword:
 		print("Correct!!")
 		break
 	else:
-		print("Incorrect, you still can try ", i - 1 ," times!")
-		i = i - 1
+		print("Incorrect")
+		if i > 0: 
+			print("you still can try ", i ," times!")
+		else:
+			print("Account is locked")
+
